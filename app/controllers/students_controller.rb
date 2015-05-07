dd22eda1abbed0e85e86de94cb27e71968b29a5f
Timothy Andrew <mail@timothyandrew.net>
2015-05-07 13:19:02 +0530

    Add students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,41 @@
 class StudentsController < ApplicationController
   def index
+    @students = Student.all
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      redirect_to student_path(@student)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    if @student.update_attributes(student_params)
+      redirect_to student_path(@student)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: "students#index"
   devise_for :users
+
+  resources :students
 end
