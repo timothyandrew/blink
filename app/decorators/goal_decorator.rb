fa7aede48ac5f71ae3987f02defb22b1b6167e29
Timothy Andrew <mail@timothyandrew.net>
2015-05-10 17:40:47 +0530

    Customize date range display based on depth.

--- app/decorators/goal_decorator.rb
+++ app/decorators/goal_decorator.rb
@@ -1,17 +1,28 @@
 class GoalDecorator < Draper::Decorator
   delegate_all
 
+  def strftime_for_depth
+    case model.depth
+    when 0
+      "%b %y"
+    when 1
+      "%b %y"
+    when 2
+      "%b %y"
+    when 3
+      "%a, %-d %B %y"
+    when 4
+      "%d %b %y"
+    else
+      "%d %b %y"
+    end
+  end
+
   def date_range
     return "" if model.start.blank? or model.end.blank?
-
-    start = model.start.strftime("%b '%y")
-    e = model.end.strftime("%b '%y")
-
-    if start == e
-      start
-    else
-      "#{start} - #{e}"
-    end
+    start = model.start.strftime(strftime_for_depth)
+    e = model.end.strftime(strftime_for_depth)
+    "#{start} - #{e}"
   end
 
   def expanded_date_range
