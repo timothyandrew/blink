246bf68935695ad7282385315100a53c915064b3
Timothy Andrew <mail@timothyandrew.net>
2015-05-24 13:59:20 +0530

    Add tests for notes.

---
+++ spec/features/notes_spec.rb
@@ -0,0 +1,31 @@
+describe "Notes", :type => :feature do
+  before(:each) do
+    @user = create(:user)
+    login(@user)
+    @student_name = create_student
+    click_on @student_name
+  end
+
+  it "saves global notes per user" do
+    notes = Faker::Lorem.paragraph
+    click_on "General Notes"
+    expect(page).to have_content "You need to add some general notes"
+
+    click_on "Edit General Notes"
+    fill_in_ckeditor "general_note_text", with: notes
+    click_on "Save"
+    expect(page).to have_content notes
+  end
+
+  it "saves notes against each student", focus: true do
+    notes = Faker::Lorem.paragraph
+    click_on "Notes"
+    expect(page).to have_content "You need to add some notes"
+
+    click_on "Edit Notes"
+    fill_in_ckeditor "student_notes", with: notes
+    click_on "Save"
+    expect(page).to have_content notes
+    expect(Student.find_by_name!(@student_name).notes).to include(notes)
+  end
+end
