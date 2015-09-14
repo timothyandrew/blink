e6ea4e560223e427eb5910cd37c4001e59d3d375
Timothy Andrew <mail@timothyandrew.net>
2015-09-14 12:30:02 +0530

    Add db backup script.

---
+++ bin/backup_db.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+SWITCH="\033["
+NORMAL="${SWITCH}0m"
+YELLOW="${SWITCH}1;33m"
+
+execute() {
+    local command=$1
+    echo
+    echo -e " ====> Running ${YELLOW} $command ${NORMAL}"
+    echo
+    eval $command
+    return $?
+}
+
+file=/tmp/`date "+%Y-%m-%dT%H:%M:%S"`.dump
+execute "dokku postgresql:dump blink blink_production > $file"
+execute "s3cmd put $file s3://blink-prod/pgbackups/production/"
+execute "curl https://nosnch.in/c77b6062f2"
