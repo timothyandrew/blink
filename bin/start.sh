5fecab9c0cb7177117cf16a02ee2c512c072ef97
Timothy Andrew <mail@timothyandrew.net>
2015-11-28 00:12:10 +0530

    Stop server before starting.

--- bin/start.sh
+++ bin/start.sh
@@ -3,4 +3,5 @@
 bundle install -j4 --without development test
 RAILS_ENV=production rake db:migrate
 RAILS_ENV=production rake assets:precompile
+RAILS_ENV=production bundle exec passenger stop
 RAILS_ENV=production RACK_ENV=production PORT=3000 foreman start
