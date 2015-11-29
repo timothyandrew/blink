d3fc4cbb2ce4d4a3183b2f726149625eb1233f9e
Timothy Andrew <mail@timothyandrew.net>
2015-11-29 20:22:11 +0530

    Update IEP template to show groups.

--- app/services/iep_template.rb
+++ app/services/iep_template.rb
@@ -6,7 +6,20 @@ class IEPTemplate
   end
 
   def generate
-    context = {student: @student, subjects: Goal.sort(Goal.roots.where(student_id: @student.id))}
+    subjects = Goal.sort(Goal.roots.where(student_id: @student.id)).map do |subject|
+      grouped_long_term_goals = subject.children.group_by do |goal|
+        goal.category_name || "<No Category>"
+      end
+      should_show_groups = grouped_long_term_goals.keys.count > 1
+      {
+        title: subject.title,
+        ltg: grouped_long_term_goals,
+        should_show_groups: should_show_groups,
+        should_not_show_groups: !should_show_groups
+      }
+    end
+
+    context = {student: @student, subjects: subjects}
     @template.render_to_string(context)
   end
 end
