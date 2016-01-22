4ef22c20a277587d91a40a688f0cc7d8a8210207
Timothy Andrew <mail@timothyandrew.net>
2016-01-22 13:38:39 +0530

    Fix housie test.

--- spec/features/housie_spec.rb
+++ spec/features/housie_spec.rb
@@ -8,6 +8,8 @@ describe "Housie", :type => :feature do
 
   it "generates a housie PDF" do
     fill_in "Number of Players", with: 10
+    fill_in "Number of Rows", with: 3
+    fill_in "Numbers Per Row", with: 4
     click_on "Default"
     click_on "Done"
     expect(download_content).to_not be_nil
