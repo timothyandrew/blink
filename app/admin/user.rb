83946a33b045d9d0c8967ddb2c255e4fefe5e9c2
Timothy Andrew <mail@timothyandrew.net>
2016-02-02 11:59:06 +0530

    Fix user admin page.

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,3 +1,21 @@
 ActiveAdmin.register User do
   actions :all, except: [:destroy]
+  permit_params :email, :name, :password, :admin
+  index do
+    selectable_column
+    column :email
+    column :name
+    column :admin
+    column :sign_in_count
+    actions
+  end
+  form do |f|
+    f.semantic_errors
+    if f.object.new_record?
+      f.inputs :email, :name, :password, :admin
+    else
+      f.inputs :email, :name, :admin
+    end
+    f.actions
+  end
 end
