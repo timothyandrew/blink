449cfe0a92394c1c276f3591757cc476b6287788
Timothy Andrew <mail@timothyandrew.net>
2016-11-04 07:59:34 +0530

    Reduce passenger concurrency.

--- bin/start.sh
+++ bin/start.sh
@@ -2,6 +2,6 @@
 
 bundle install -j4 --without development test
 RAILS_ENV=production bundle exec rake db:migrate
-RAILS_ENV=production bundle exec rake assets:precompile
+RAILS_ENV=production bundle execrake assets:precompile
 RAILS_ENV=production bundle exec passenger stop --port 3000
-RAILS_ENV=production bundle exec passenger start -p 3000 -e production -d
+RAILS_ENV=production bundle exec passenger start -p 3000 -e production -d  --max-pool-size 3
